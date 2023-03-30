85133fdd7decd20c093d0df2e5b38ff5754a24ff
Martel <jmartel@b1128-01.cmaisonneuve.qc.ca>
2023-03-30 16:56:38 -0400

    Fin du cours 1 React

--- src/Entete/Entete.jsx
+++ src/Entete/Entete.jsx
@@ -1,12 +1,46 @@
+import { useState } from 'react';
 import './Entete.css';
 
 export default function Entete(){
+    let [courriel, setCourriel] = useState("");
+    let [connecter, setConnecter] = useState(false);
 
+    function verifierCourriel(){
+        console.log(courriel);
+        if(courriel && !connecter){
+            setConnecter(true);
+        }
+        else if(courriel && connecter){
+            setConnecter(false);
+        }
+
+        // Mais comment lire le champ input?
+
+    }
+
+   function etatConnection()
+   {
+        let html = <p>Pas connecté</p>;
+        if(connecter){
+            html = <p>Est connecté</p>;
+        }
+        return html
+   }
+    function btnConnection(){
+        let chaine = "Se connecter";
+        if(connecter){
+            html = "Se déconnecter";
+        }
+        return chaine;
+    }
     return (
-        <header className='entete'>    
+        <header id='entete' className='entete'>    
             <h1>Titre de page</h1>
             <span className='spacer'></span>
             <nav>
+                Courriel<input disabled={connecter} value={courriel} onChange={(e)=>{setCourriel(e.target.value)}} type="text"></input>
+                <button onClick={(e)=>{ verifierCourriel()}}>{(!connecter ? "Se connecter" : "Se déconnecter" )}</button>
+                {etatConnection()}
                 <a href="">Accueil</a>
                 <a href="">Liste</a>
             </nav>
